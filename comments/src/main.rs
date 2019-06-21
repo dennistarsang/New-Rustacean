670f50b90f8df4fdce3d3af0cfceac83d9fcc48e
Dennis Sang <sangdennis9@gmail.com>
2019-06-21 07:30:02 +0300

    A Rustacean's learning curve..Smooth

---
+++ comments/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    //This is a single line comment
+    /*
+    This
+    is
+    a
+    multi-line
+    comment
+    */
+    //The macro below prints 'hello world' to the console
+    println!("Hello, world!");
+}
---
+++ variables/src/main.rs
@@ -0,0 +1,15 @@
+fn main() {
+    //Variable Basics
+    let mut x = 45; //Variable binding
+
+    println!("The value of x is {}", x);
+
+    x = 69; // Copy trait
+
+    println!("The value of x is {}", x);
+
+    //Variables
+    let y: u64 = 69; //u64
+    let f: f32 = 6.9; //f32
+    let b: bool = false;
+}
